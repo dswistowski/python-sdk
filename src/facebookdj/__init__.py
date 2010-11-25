2e4f79b114ec87e46a902fedd4532c50a301ff40
Damian Świstowski <damian@swistowski.org>
2010-11-25 01:03:38 +0100

    bad resolution, but work. decorators must be finished, but graph and rest apis works ok

--- src/facebookdj/__init__.py
+++ src/facebookdj/__init__.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-import facebook
-import base64
+from facebook.graph import GraphAPI, get_user_from_cookie
+from facebook.rest import Facebook
+import base64, re
 from django.utils import simplejson
+from django.http import HttpResponse
+
 
 try:
     from threading import local
@@ -19,7 +22,12 @@ def get_graph_client():
         return _thread_locals.graph
     except AttributeError:
         raise ImproperlyConfigured('Make sure you have the Facebook middleware installed.')
-    
+
+def get_rest_client():
+    try:
+        return _thread_locals.rest
+    except AttributeError:
+        raise ImproperlyConfigured('Make sure you have the Facebook middleware installed.')    
 
 def require_login(next=None):
     def decorator(view):
@@ -27,8 +35,7 @@ def require_login(next=None):
             next = newview.next
             
             try:
-                graph = request.graph
-                print "user.id", graph.uid
+                graph = request.graph                
             except:
                 raise ImproperlyConfigured('Make sure you have Facebook middleware installed')
             
@@ -37,10 +44,21 @@ def require_login(next=None):
         return newview
     return decorator
 
-class Graph(facebook.GraphAPI):
+class Graph(GraphAPI):
     def __init__(self, access_token=None, user_id=None):
         super(Graph, self).__init__(access_token)
         self.uid = user_id
+        
+    def redirect(self, url):
+        """
+        Helper for Django which redirects to another page. If inside a
+        canvas page, writes a <fb:redirect> instead to achieve the same effect.
+
+        """
+        if re.search("^https?:\/\/([^\/]*\.)?facebook\.com(:\d+)?", url.lower()):
+            return HttpResponse('<script type="text/javascript">\ntop.location.href = "%s";\n</script>' % url)
+        else:
+            return HttpResponseRedirect(url)
 
 
 def base64_url_decode(input):
@@ -71,14 +89,16 @@ current thread by using get_graph_client().
             user_id = data.get('user_id')
             
         else:
-            cookie = facebook.get_user_from_cookie(request.COOKIES, self.api_key, self.secret_key)
+            cookie = get_user_from_cookie(request.COOKIES, self.api_key, self.secret_key)
             if cookie:
                 access_token = cookie['access_token']
                 user_id = cookie.get('uid')
             else:
                 access_token = None
                 user_id = None
+        
         _thread_locals.graph = request.graph = Graph(access_token, user_id)
+        _thread_locals.rest = request.rest = Facebook(self.api_key, self.secret_key, access_token)
         
         
 
